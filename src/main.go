c5b1d63e0ee95ac0c7fff5e7b2287c8b29f50ec9
dara.dadachanji <dara.dadachanji@irhythmtech.com>
2022-01-20 21:56:06 -0800

    added option to toggle showing all guesses

--- src/main.go
+++ src/main.go
@@ -21,17 +21,18 @@ func main() {
 	} else if flags.Detailed {
 		CalculateBestGuesses(flags.Start, flags.End)
 	} else {
-		HelpGuess()
+		HelpGuess(flags.ShowAllGuesses)
 	}
 
 }
 
 type Flags struct {
-	Guess    string
-	Detailed bool
-	Rough    bool
-	Start    int
-	End      int
+	Guess          string
+	Detailed       bool
+	Rough          bool
+	ShowAllGuesses bool
+	Start          int
+	End            int
 }
 
 func getFlags() Flags {
@@ -44,21 +45,26 @@ func getFlags() Flags {
 		"detailed",
 		false,
 		"perform a detailed scoring of guesses from the rough list")
+	showAllGuesses := flag.Bool(
+		"allguesses",
+		false,
+		"show all guesses and scores after each hint instead of just the ones tied for best")
 	start := flag.Int("start", 0, "starting point of for best guesses")
 	end := flag.Int("end", 0, "ending point for best guesses")
 	rough := flag.Bool("rough", false, "calculate alphabet and rough guess scores")
 	flag.Parse()
 	flags := Flags{
-		Guess:    *guess,
-		Detailed: *detailed,
-		Rough:    *rough,
-		Start:    *start,
-		End:      *end,
+		Guess:          *guess,
+		Detailed:       *detailed,
+		Rough:          *rough,
+		Start:          *start,
+		End:            *end,
+		ShowAllGuesses: *showAllGuesses,
 	}
 	return flags
 }
 
-func HelpGuess() {
+func HelpGuess(showAllGuesses bool) {
 	guesser := Guesser{Answers: &game.AllAnswers, Guesses: &game.AllGuesses}
 	fmt.Printf("Suggested first guess: %s\n", firstGuess)
 	for {
@@ -75,7 +81,7 @@ func HelpGuess() {
 			}
 			fmt.Printf("Narrowed to %d answers\n", len(*guesser.Answers))
 			if len(*guesser.Answers) > 2 {
-				suggestions := guesser.SuggestGuess(true)
+				suggestions := guesser.SuggestGuess(showAllGuesses)
 				for _, suggestion := range suggestions {
 					averageOptions := float64(suggestion.Value) / float64(len(*guesser.Answers))
 					fmt.Printf("Suggested Guess: %s, aggregate score %d, average remaining answers: %.2f\n", suggestion.Key, suggestion.Value, averageOptions)
